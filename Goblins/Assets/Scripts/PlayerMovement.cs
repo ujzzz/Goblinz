25e6082a0d5074f76a816efa4de3a568799c549a
Alex Braylan <alexbraylan@gmail.com>
2016-09-17 19:13:23 -0500

    unpublic

--- Goblins/Assets/Scripts/PlayerMovement.cs
+++ Goblins/Assets/Scripts/PlayerMovement.cs
@@ -62,7 +62,7 @@ public class PlayerMovement : MonoBehaviour {
 		}
 	}
 
-	public void PlayerRotate () {
+	private void PlayerRotate () {
 		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 		RaycastHit hit;
 		if (Physics.Raycast (ray, out hit)) {
